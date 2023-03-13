cb5811a1ab545265a1b2ff32ce9c26562444ceb6
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-13 21:49:06 +0300

    nefiasco

--- Examples/Example005_ifelse/Program.cs
+++ Examples/Example005_ifelse/Program.cs
@@ -1,11 +1,15 @@
 ﻿Console.WriteLine("Enter your name:");
-string? username = Console.ReadLine();
-
-if(username.ToLower == mary);
-{
-    Console.WriteLine("Hello Mary!");
-}
-else 
-{
-    Console.WriteLine("Hello " + username);
-}
+string username = Console.ReadLine();
+//Console.WriteLine("Hello Mary!");
+//DisplayWeatherReport();
+//void DisplayWeatherReport()
+//{
+    if(username?.ToLower() == "mary")
+    {
+   Console.WriteLine("Hello Mary!");
+    }
+    else
+    {
+        Console.WriteLine("Hello " + username);
+    }
+//}
