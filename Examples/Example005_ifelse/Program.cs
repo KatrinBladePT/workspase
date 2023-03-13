43ead7d743d692d3d5fc09b61068935c0a007b18
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-13 13:30:11 +0300

    fiasco

---
+++ Examples/Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name:");
+string? username = Console.ReadLine();
+
+if(username.ToLower == mary);
+{
+    Console.WriteLine("Hello Mary!");
+}
+else 
+{
+    Console.WriteLine("Hello " + username);
+}
