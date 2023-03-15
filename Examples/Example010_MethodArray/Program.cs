87abdab7919a35a84f4868ba21a706d3720995b4
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-03-15 03:54:41 +0300

    ZDECSH

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[]array = {45, 34, 42, 4, 22, 13, 5, 3};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+        {
+        Console.WriteLine(index);
+        break;
+        }
+    index++;
+}
+        
